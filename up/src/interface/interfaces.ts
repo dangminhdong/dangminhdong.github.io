6e6a6da2b40347745f00a11750c3b8139a1f4a48
Đặng Minh Đông <137901065+dangminhdong@users.noreply.github.com>
2025-07-06 17:10:47 +0700

    Delete up directory

--- up/src/interface/interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface ImgurOptions {
-    clientid: string;
-    callback?: (data: any) => void;
-    onLoading?: () => void;
-    onSuccess?: (data: any) => void;
-    onSuccessAll?: (data: any) => void;
-    onError?: (error: any) => void;
-}
